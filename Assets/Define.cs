56ed140d5c7c2749b114892c0b099e67275a893e
soso03185 <34054994+soso03185@users.noreply.github.com>
2021-11-12 17:33:13 +0900

    PlayerScript

---
+++ Assets/Define.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+    public enum CharacterState
+    {
+        Idle,
+        Moving,
+        Skill,
+        Jump,
+        Dash,
+        WallJump,
+        Dead,
+    }
+
+    public enum MoveDir
+    {
+        None,
+        Up,
+        Down,
+        Left,
+        Right,
+    }
+    public enum Layer
+    {
+        Block = 6,
+        Floor = 7,
+        Player = 10,
+    }
+}
