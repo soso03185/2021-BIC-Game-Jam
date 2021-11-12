b25fa1ef4c4007e546b3983646b5037d5caf4b06
soso03185 <34054994+soso03185@users.noreply.github.com>
2021-11-12 20:55:23 +0900

    PlayerMove_Script

---
+++ Assets/Define.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Define
+{
+    public enum CharacterState
+    {
+        Idle,
+        Moving,
+        Run,
+        Dead,
+    }
+
+    public enum MoveDir
+    {
+        None,
+        Left,
+        Right,
+    }
+}
